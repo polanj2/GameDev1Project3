be820b0f18fa7833a0db36d89e046b79dd53a9d6
Axel <stewaa5@rpi.edu>
2017-10-30 02:37:54 -0400

    Added PureData folder, OSC sender scripts, OSCSharp library, made all objects children of a World gameObject, and applied the OSC sender scripts to the appropriate objects. I hope I didn't ruin the project in the process - it seems okay on my end, but there were a lot of added files and I didn't remove the ones it deleted automatically.

---
+++ Assets/OSCMaster.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using OSCsharp.Net;
+using OSCsharp.Data;
+
+public class OSCMaster : MonoBehaviour {
+
+    public UDPTransmitter oscTransmitter;
+    public OscMessage controllerOscMessage;
+    private string footNameSpace = "/foot";
+    private string timeNameSpace = "/time";
+    private string distNameSpace = "/dist";
+
+    // Use this for initialization
+    void Start ()
+    {
+        oscTransmitter = new UDPTransmitter("127.0.0.1", 6666);
+        oscTransmitter.Connect();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void sendFootStepOSC(string A)
+    {
+        controllerOscMessage = new OscMessage(footNameSpace);
+        controllerOscMessage.Append(A);
+        oscTransmitter.Send(controllerOscMessage);
+    }
+
+    public void sendTime(float time)
+    {
+        controllerOscMessage = new OscMessage(timeNameSpace);
+        controllerOscMessage.Append(time);
+        oscTransmitter.Send(controllerOscMessage);
+    }
+
+    public void sendDist(float dist)
+    {
+        controllerOscMessage = new OscMessage(distNameSpace);
+        controllerOscMessage.Append(dist);
+        oscTransmitter.Send(controllerOscMessage);
+    }
+}
---
+++ Assets/SendData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SendData : MonoBehaviour {
+
+    public OSCMaster master;
+
+	// Use this for initialization
+	void Start () {
+        GameObject myWorld = GameObject.Find("World");
+        master = (OSCMaster)myWorld.GetComponent<OSCMaster>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // Demo data send of distance between self and world center
+        master.sendDist(Vector3.Distance(master.gameObject.transform.position, gameObject.transform.position));
+	}
+}
